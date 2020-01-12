df353ab747af004ad6348a618aeca21dd5a959e6
devafri <49462283+devafri@users.noreply.github.com>
2020-01-12 12:28:40 +0800

    Update lc.js

--- js/lc.js
+++ js/lc.js
@@ -1,5 +1,5 @@
 var lc = LC.init(document.getElementById("lc"), {
-    imageURLPrefix: '/img',
+    imageURLPrefix: '../img',
     toolbarPosition: 'bottom',
     defaultStrokeWidth: 2,
     strokeWidths: [1, 2, 3, 5, 30]
